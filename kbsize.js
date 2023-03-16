cfc51813b5a0302cd4abcb13f4fc5c994dd3884a
Salvador Aceves <salvador@aceves.mx>
2023-03-15 23:54:43 -0500

    work with legacy <12 versions of node

--- kbsize.js
+++ kbsize.js
@@ -1,7 +1,13 @@
 const { Blob } = require('buffer');
+const legacyBlob = require('cross-blob');
 
 const sizeInKB = str => {
-  const blob = new Blob([str]);
+  let blob;
+  try {
+    blob = new Blob([str]);
+  } catch (err) {
+    blob = new legacyBlob([str]);
+  }
   const sizeInBytes = blob.size;
   const sizeInKilobytes = sizeInBytes / 1024;
   return sizeInKilobytes;
